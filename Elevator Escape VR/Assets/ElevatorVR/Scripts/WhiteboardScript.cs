012e92706e2c165aef893524c117fb0c724cbfba
RossH6348 <75311103+RossH6348@users.noreply.github.com>
2023-03-08 11:56:20 +0000

    Fixed whiteboard bug

--- Elevator Escape VR/Assets/ElevatorVR/Scripts/WhiteboardScript.cs
+++ Elevator Escape VR/Assets/ElevatorVR/Scripts/WhiteboardScript.cs
@@ -10,16 +10,33 @@ public class WhiteboardScript : MonoBehaviour
     public Texture2D whiteboardTex;
 
     //Applying the whiteboard texture.
-    private void Start()
+    private void Awake()
     {
         Renderer render = GetComponent<Renderer>();
         whiteboardTex = new Texture2D((int)whiteboardRes.x, (int)whiteboardRes.y, TextureFormat.RGBA32, false);
+
+        Color[] pixels = new Color[(int)whiteboardRes.x * (int)whiteboardRes.y];
+        for (int i = ((int)whiteboardRes.x * (int)whiteboardRes.y) - 1; i > -1; i--)
+            pixels[i].r = pixels[i].g = pixels[i].b = (137.0f / 255.0f);
+
+        whiteboardTex.SetPixels(0, 0, (int)whiteboardRes.x, (int)whiteboardRes.y, pixels);
+        whiteboardTex.Apply();
+
         render.material.mainTexture = whiteboardTex;
     }
 
     public void clearWhiteboard()
     {
         if (whiteboardTex != null)
+        {
             whiteboardTex.Reinitialize((int)whiteboardRes.x, (int)whiteboardRes.y, TextureFormat.RGBA32, false);
+
+            Color[] pixels = new Color[(int)whiteboardRes.x * (int)whiteboardRes.y];
+            for (int i = ((int)whiteboardRes.x * (int)whiteboardRes.y) - 1; i > -1; i--)
+                pixels[i].r = pixels[i].g = pixels[i].b = (137.0f / 255.0f);
+
+            whiteboardTex.SetPixels(0, 0, (int)whiteboardRes.x, (int)whiteboardRes.y, pixels);
+            whiteboardTex.Apply();
+        }
     }
 }
